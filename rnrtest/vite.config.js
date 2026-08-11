823fa41b36d702f0ed754f954654a6052705547f
Manish Kewat <manish.kewat@sunbrilotechnologies.com>
2026-08-11 18:41:03 +0530

    change Host

--- rnrtest/vite.config.js
+++ rnrtest/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     allowedHosts: [
-      "rnr-test.sunbrilotechnologies.com",
+      "rnr.sunbrilotechnologies.com",
     ],
   },
 });
